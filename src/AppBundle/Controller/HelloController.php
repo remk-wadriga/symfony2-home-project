3fff4c46a8745311efa099aee7c66aee1b4d1e74
rem <remkwadiga@yandex.ua>
2015-03-10 22:54:46 +0200

    Controller capt

---
+++ src/AppBundle/Controller/HelloController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace AppBundle\Controller;
+
+/**
+ * Created by PhpStorm.
+ * User: Dima
+ * Date: 10.03.2015
+ * Time: 19:51
+ */
+class HelloController
+{
+
+} 
--- src/AppBundle/Controller/RandomController.php
+++ src/AppBundle/Controller/RandomController.php
@@ -3,6 +3,8 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 /**
  * Created by PhpStorm.
@@ -14,8 +16,21 @@ class RandomController extends Controller
 {
     public function indexAction($limit)
     {
-        return $this->render('AppBundle:Random:index.html.twig', [
+        return $this->render('AppBundle:Random/Test:index.html.twig', [
             'number' => rand(1, $limit),
         ]);
     }
-} 
+
+    /**
+     * @param string $firstName
+     * @param string $lastName
+     * @return Response
+     * @Route("/hello/{firstName}/{lastName}", name="hello")
+     */
+    public function testAction($firstName, $lastName)
+    {
+        $request = $this->getRequest();
+        $page = $request->query->get('page', 1);
+        return new Response('Hello, '.$firstName.' '.$lastName.' '.$page.'!');
+    }
+}
